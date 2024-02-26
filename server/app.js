11138aa04f63e3c0e38932d76296fe4d213721b9
Mohammad Junaid Ahmed <ahmedjunaid2004@gmail.com>
2024-02-26 09:30:15 +0530

    Initialized Backend

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+require('dotenv/config');
+const mongoose = require('mongoose')
 
-app.listen(3000,'0.0.0.0', ()=>{
-    console.log('server is running http://localhost:3000');
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+    dbName: 'careerclarify-database'
+})
+.then(()=>{
+    console.log("Database connection is ready!")
+    app.listen(3000,'0.0.0.0', ()=>{
+        console.log('server is running http://localhost:3000');
+    })
 })
