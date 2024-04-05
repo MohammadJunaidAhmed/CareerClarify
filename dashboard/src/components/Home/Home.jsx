45c39c15233a26bb4349081aed77b62d3f8b1614
Mohammad Junaid Ahmed <ahmedjunaid2004@gmail.com>
2024-04-05 09:47:32 +0530

    Added dropdown functionality in home

--- dashboard/src/components/Home/Home.jsx
+++ dashboard/src/components/Home/Home.jsx
@@ -5,8 +5,11 @@ import SalesTotal from "./Cards/SalesTotal"
 import calender from "../../assets/calendar.png"
 import downArrow from "../../assets/down-arrow.png"
 import TotalOrders from "./Cards/ChartCards/TotalOrders"
+import { useState } from "react"
 
 const Home = () => {
+  const [dataFormat, setDataFormat] = useState('m');
+  const [isDataFormatOpen, setIsDataFormatOpen] = useState(false);
   return (
     <div className="w-full h-full flex">
       <div className="flex-1 bg-[#E0E0E0] flex flex-col">
@@ -15,12 +18,31 @@ const Home = () => {
           <div className=" w-full h-20 flex justify-between">
             <h1 className="text-3xl flex items-center justify-center">Overview</h1>
 
-            <div className="h-full w-56 flex items-center justify-center p-2"> {/*TODO: UPDATE UI HERE*/}
+            <div className="h-full w-56 items-center justify-center p-2 flex flex-col relative cursor-pointer"  onClick={()=>{setIsDataFormatOpen(!isDataFormatOpen)}}> {/*TODO: UPDATE UI HERE*/}
               <div className="h-1/2 w-full text-lg bg-[#F5F5F5] text-[#7A7A7A] flex justify-center items-center gap-4 rounded-lg border-2 border-black">
                 <img src={calender} className="h-2/3"/>
-                <h1>Last 30 Days</h1>
+                <h1>
+                  {
+                    dataFormat === 'w' && <h1> Last 7 Days</h1>
+                    ||
+                    dataFormat === 'm' && <h1> Last 30 Days</h1>
+                    || 
+                    dataFormat === 'y' && <h1> Last 365 Days</h1>
+                  }
+                </h1>
                 <img src={downArrow} className="h-2/3"/>
               </div>
+              {
+                isDataFormatOpen &&
+                <div className="w-full absolute text-lg text-[#7A7A7A] top-12 mt-1 p-2 flex">
+                  <div className="bg-slate-400 flex-1 flex flex-col rounded-xl">
+                    <div className=" px-2 hover:bg-[#F5F5F5]" onClick={()=>{setDataFormat('w')}}>Weekly</div>
+                    <div className=" px-2 hover:bg-[#F5F5F5]" onClick={()=>{setDataFormat('m')}}>Monthly</div>
+                    <div className=" px-2 hover:bg-[#F5F5F5]" onClick={()=>{setDataFormat('y')}}>Yearly</div>
+                  </div>
+                </div>
+
+              }
             </div>
           </div>
           {/* MAIN CONTENT BELOW */}
@@ -34,7 +56,8 @@ const Home = () => {
                 <PendingOrders/>
                 <SalesTotal/>
               </div>
-              <div className="w-full flex-1 p-2">
+              <div className="w-full flex-1 p-2 flex gap-5">
+                <TotalOrders/>
                 <TotalOrders/>
               </div>
             </div>
